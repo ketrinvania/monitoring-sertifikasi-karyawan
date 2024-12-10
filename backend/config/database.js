a2796c6a30d3dae0e5dcd2337c46c901bbaeb7ad
root <root@Ketrinvani>
2024-12-10 23:04:11 +0700

    Menambahkan konfigurasi database dan file pendukung

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,9 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('monitoring_db','root','',{
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
+const db = new Sequelize('monitoring_db', 'root', 'password12345', {
+    host: '127.0.0.1',
+    dialect: 'mysql'
 });
+
 
 export default db;
